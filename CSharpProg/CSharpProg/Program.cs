da78dbe7277723c98fc9eca43bc52fdd0e512284
stories282 <stories282@gmail.com>
2020-02-07 10:40:40 +0900

    파라미터를 받고 상황에 따라 반환값이 다른 c# 프로그램 작성

---
+++ CSharpProg/CSharpProg/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProg
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return 2;
+            }
+            string arg1 = args[0];
+            int arg2 = 0;
+            if (int.TryParse(args[1], out arg2))
+            {
+                Console.WriteLine("args {0} {1}", arg1, arg2);
+
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
